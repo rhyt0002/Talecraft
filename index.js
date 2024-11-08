a04e58d56120bcc6904f48d0fe02aa71ad88e7bd
Rhythm <rhyt0002@algonquinlive.com>
2024-11-08 16:17:52 -0500

    contact forum added

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ document.querySelectorAll('.faq h3').forEach(faq => {
         arrow.innerHTML = answer.style.display === 'block' ? '&#9650;' : '&#9660;';
     });
 });
+function sendEmail() {
+    let contactrhea = {
+        name: document.getElementById('fullname').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value
+    }
+// need to change service Id
+    emailjs.send("service_x5d40gc", "Rhea-contact-forum", contactrhea).then(
+        alert('Message sent successfully!'))
+        .catch(function(error) {
+            alert('Failed to send message. Please try again later.');
+            console.error(error);
+        });
+    }
